cbea504678460055ea4ce1626470f991aa42b2c4
Loïc Bovay <loic.bovay@akson.ch>
2017-04-25 17:25:45 +0200

    Added sleep functions from sleepydog library amongst other things

--- src/feather-32u4.cpp
+++ src/feather-32u4.cpp
@@ -1,6 +1,23 @@
 #include "feather-32u4.h"
-#include "Arduino.h"
+
+void led_on(){
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void led_off(){
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
+}
 
 float battery_voltage(){
   return analogRead(PIN_VBAT)/1024.0*6.6;
+}
+
+void sleep(int sleep_ms) {
+    int sleepMS =0;
+
+    while(sleepMS <sleep_ms) {
+        sleepMS +=Watchdog.sleep(min(sleep_ms -sleepMS, WD_MAX));
+    }
 }
--- src/feather-32u4.h
+++ src/feather-32u4.h
@@ -1,12 +1,20 @@
 #ifndef _FEATHER_H_
 #define _FEATHER_H_
 
+#include <Adafruit_SleepyDog.h>
+#include "Arduino.h"
+
 #define LED_BUILTIN   13
 #define PIN_VBAT      A9
-#define PIN_RFM95_NSS 8
-#define PIN_RFM95_IO0 7
-#define PIN_RFM95_IO1 6
+#define WD_MAX        8000      //Max sleep time to avoid watchdog reset
+
+#define PIN_RFM96_NSS 8
+#define PIN_RFM96_IO0 7
+
+void led_on();
+void led_off();
 
 float battery_voltage();
+void sleep(int sleep_ms);
 
 #endif
